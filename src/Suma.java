e72e8019757b2e601dc13c3fe4b072e8da6a8d2f
aabril86 <aabril@elpuig.xeill.net>
2021-01-31 18:39:48 +0100

    javadoc

---
+++ src/Suma.java
@@ -0,0 +1,20 @@
+
+public class Suma {
+    int n1, n2;
+
+    public Suma(int n1, int n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+
+    /**
+     *
+     * @param num1 primer parametro
+     * @param num2 segundo parametro
+     * @return resultado de la suma de los digitos
+     */
+    public int sumar(int num1, int num2){
+        return num1 + num2;
+    }
+
+}
