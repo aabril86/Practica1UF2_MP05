656ec7c781fb65473790ad7514c136817946ac5b
aabril86 <aabril@elpuig.xeill.net>
2021-01-31 16:39:16 +0100

    prueba4

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mon");
         System.out.println("Prueba 2");
+        System.out.println("Prueba 3");
     }
 }
